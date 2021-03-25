22bf98df59459445a65d4592b585aaf498052b52
jose <zedomel@gmail.com>
2021-03-25 01:21:38 -0300

    Fix heatmap

--- src/index.js
+++ src/index.js
@@ -10,16 +10,20 @@ var map = L.map('map',{center: [53.15, -6.7], zoom: 10});
 // OSM Baselayer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
-// var pointStyle = {
-//     radius: 2,
-//     fillColor: "#000000",
-//     color: "#000000",
-//     weight: 1,
-//     fillOpacity: 1
-// };
+var pointStyle = {
+    radius: 2,
+    fillColor: "#000000",
+    color: "#000000",
+    weight: 1,
+    fillOpacity: 1
+};
 var points = new L.GeoJSON.AJAX('globi-obs.geojson', {
     middleware:function(data){
-        console.log(data);
-        return L.heatLayer(points, {radius:12,blur:25,maxZoom:11})        
+        coords = []; //define an array to store coordinates
+        L.geoJSON(data, {
+            pointToLayer: (feature, latlng) => L.circleMarker(latlng, pointStyle),
+            onEachFeature: (feature, layer) => coords.push(feature.geometry.coordinates),
+            coordsToLatLng: coords => L.heatLayer(coords, {radius:12,blur:25,maxZoom:11})            
+        });        
     }
 }).addTo(map);
